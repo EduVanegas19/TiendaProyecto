1efb2f156c416458a2ed4228ffc03da69b37408f
Legalmenteluis4 <101478478+Legalmenteluis4@users.noreply.github.com>
2023-06-11 23:02:03 -0600

    PUTO EL QUE LO LEA
    Jajajaja sos puto

--- TIENDA/TIENDA/GUI/Main.Designer.cs
+++ TIENDA/TIENDA/GUI/Main.Designer.cs
@@ -42,6 +42,13 @@ namespace TIENDA.GUI
             this.lblUsuario = new System.Windows.Forms.ToolStripStatusLabel();
             this.lblRol = new System.Windows.Forms.ToolStripStatusLabel();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
             this.btnPedidos.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.frmGestionReporte)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.frmGestionDetallePedido)).BeginInit();
@@ -50,31 +57,44 @@ namespace TIENDA.GUI
             ((System.ComponentModel.ISupportInitialize)(this.frmGestionCliente)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnVentas)).BeginInit();
             this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
             this.SuspendLayout();
             // 
             // btnPedidos
             // 
             this.btnPedidos.BackColor = System.Drawing.Color.White;
             this.btnPedidos.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnPedidos.BackgroundImage")));
+            this.btnPedidos.Controls.Add(this.pictureBox6);
+            this.btnPedidos.Controls.Add(this.pictureBox7);
+            this.btnPedidos.Controls.Add(this.pictureBox5);
+            this.btnPedidos.Controls.Add(this.pictureBox4);
+            this.btnPedidos.Controls.Add(this.pictureBox3);
+            this.btnPedidos.Controls.Add(this.pictureBox2);
+            this.btnPedidos.Controls.Add(this.pictureBox1);
             this.btnPedidos.Controls.Add(this.frmGestionReporte);
             this.btnPedidos.Controls.Add(this.frmGestionDetallePedido);
             this.btnPedidos.Controls.Add(this.frmGestionProveedor);
             this.btnPedidos.Controls.Add(this.frmGestionEmpleado);
             this.btnPedidos.Controls.Add(this.frmGestionCliente);
             this.btnPedidos.Controls.Add(this.btnVentas);
-            this.btnPedidos.Location = new System.Drawing.Point(0, 79);
-            this.btnPedidos.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnPedidos.Location = new System.Drawing.Point(0, 64);
+            this.btnPedidos.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnPedidos.Name = "btnPedidos";
-            this.btnPedidos.Size = new System.Drawing.Size(1228, 583);
+            this.btnPedidos.Size = new System.Drawing.Size(921, 474);
             this.btnPedidos.TabIndex = 13;
             // 
             // frmGestionReporte
             // 
             this.frmGestionReporte.Image = ((System.Drawing.Image)(resources.GetObject("frmGestionReporte.Image")));
-            this.frmGestionReporte.Location = new System.Drawing.Point(455, 274);
-            this.frmGestionReporte.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.frmGestionReporte.Location = new System.Drawing.Point(187, 223);
             this.frmGestionReporte.Name = "frmGestionReporte";
-            this.frmGestionReporte.Size = new System.Drawing.Size(133, 123);
+            this.frmGestionReporte.Size = new System.Drawing.Size(100, 100);
             this.frmGestionReporte.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.frmGestionReporte.TabIndex = 16;
             this.frmGestionReporte.TabStop = false;
@@ -83,10 +103,9 @@ namespace TIENDA.GUI
             // frmGestionDetallePedido
             // 
             this.frmGestionDetallePedido.Image = ((System.Drawing.Image)(resources.GetObject("frmGestionDetallePedido.Image")));
-            this.frmGestionDetallePedido.Location = new System.Drawing.Point(163, 274);
-            this.frmGestionDetallePedido.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.frmGestionDetallePedido.Location = new System.Drawing.Point(39, 223);
             this.frmGestionDetallePedido.Name = "frmGestionDetallePedido";
-            this.frmGestionDetallePedido.Size = new System.Drawing.Size(133, 123);
+            this.frmGestionDetallePedido.Size = new System.Drawing.Size(100, 100);
             this.frmGestionDetallePedido.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.frmGestionDetallePedido.TabIndex = 15;
             this.frmGestionDetallePedido.TabStop = false;
@@ -95,10 +114,9 @@ namespace TIENDA.GUI
             // frmGestionProveedor
             // 
             this.frmGestionProveedor.Image = ((System.Drawing.Image)(resources.GetObject("frmGestionProveedor.Image")));
-            this.frmGestionProveedor.Location = new System.Drawing.Point(751, 53);
-            this.frmGestionProveedor.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.frmGestionProveedor.Location = new System.Drawing.Point(350, 43);
             this.frmGestionProveedor.Name = "frmGestionProveedor";
-            this.frmGestionProveedor.Size = new System.Drawing.Size(133, 123);
+            this.frmGestionProveedor.Size = new System.Drawing.Size(100, 100);
             this.frmGestionProveedor.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.frmGestionProveedor.TabIndex = 14;
             this.frmGestionProveedor.TabStop = false;
@@ -107,10 +125,9 @@ namespace TIENDA.GUI
             // frmGestionEmpleado
             // 
             this.frmGestionEmpleado.Image = ((System.Drawing.Image)(resources.GetObject("frmGestionEmpleado.Image")));
-            this.frmGestionEmpleado.Location = new System.Drawing.Point(455, 53);
-            this.frmGestionEmpleado.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.frmGestionEmpleado.Location = new System.Drawing.Point(187, 43);
             this.frmGestionEmpleado.Name = "frmGestionEmpleado";
-            this.frmGestionEmpleado.Size = new System.Drawing.Size(133, 123);
+            this.frmGestionEmpleado.Size = new System.Drawing.Size(100, 100);
             this.frmGestionEmpleado.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.frmGestionEmpleado.TabIndex = 13;
             this.frmGestionEmpleado.TabStop = false;
@@ -119,10 +136,9 @@ namespace TIENDA.GUI
             // frmGestionCliente
             // 
             this.frmGestionCliente.Image = ((System.Drawing.Image)(resources.GetObject("frmGestionCliente.Image")));
-            this.frmGestionCliente.Location = new System.Drawing.Point(163, 53);
-            this.frmGestionCliente.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.frmGestionCliente.Location = new System.Drawing.Point(39, 43);
             this.frmGestionCliente.Name = "frmGestionCliente";
-            this.frmGestionCliente.Size = new System.Drawing.Size(133, 123);
+            this.frmGestionCliente.Size = new System.Drawing.Size(100, 100);
             this.frmGestionCliente.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.frmGestionCliente.TabIndex = 12;
             this.frmGestionCliente.TabStop = false;
@@ -131,10 +147,9 @@ namespace TIENDA.GUI
             // btnVentas
             // 
             this.btnVentas.Image = ((System.Drawing.Image)(resources.GetObject("btnVentas.Image")));
-            this.btnVentas.Location = new System.Drawing.Point(-137, -78);
-            this.btnVentas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnVentas.Location = new System.Drawing.Point(-103, -63);
             this.btnVentas.Name = "btnVentas";
-            this.btnVentas.Size = new System.Drawing.Size(133, 123);
+            this.btnVentas.Size = new System.Drawing.Size(100, 100);
             this.btnVentas.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.btnVentas.TabIndex = 11;
             this.btnVentas.TabStop = false;
@@ -145,11 +160,11 @@ namespace TIENDA.GUI
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.lblUsuario,
             this.lblRol});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 665);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 541);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 13, 0);
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 10, 0);
             this.statusStrip1.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.statusStrip1.Size = new System.Drawing.Size(1228, 30);
+            this.statusStrip1.Size = new System.Drawing.Size(921, 24);
             this.statusStrip1.TabIndex = 14;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -157,13 +172,13 @@ namespace TIENDA.GUI
             // 
             this.lblUsuario.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Right;
             this.lblUsuario.Name = "lblUsuario";
-            this.lblUsuario.Size = new System.Drawing.Size(75, 24);
+            this.lblUsuario.Size = new System.Drawing.Size(60, 19);
             this.lblUsuario.Text = "USUARIO";
             // 
             // lblRol
             // 
             this.lblRol.Name = "lblRol";
-            this.lblRol.Size = new System.Drawing.Size(36, 24);
+            this.lblRol.Size = new System.Drawing.Size(29, 19);
             this.lblRol.Text = "ROL";
             // 
             // panel1
@@ -171,27 +186,104 @@ namespace TIENDA.GUI
             this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1228, 74);
+            this.panel1.Size = new System.Drawing.Size(921, 60);
             this.panel1.TabIndex = 15;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(350, 223);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(506, 223);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 18;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(654, 43);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 19;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(506, 43);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 20;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(654, 223);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 21;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(790, 223);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox6.TabIndex = 23;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox7.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox7.Image")));
+            this.pictureBox7.Location = new System.Drawing.Point(790, 43);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox7.TabIndex = 22;
+            this.pictureBox7.TabStop = false;
             // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.BackColor = System.Drawing.Color.Silver;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1228, 695);
+            this.ClientSize = new System.Drawing.Size(921, 565);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.btnPedidos);
             this.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.IsMdiContainer = true;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.Name = "Main";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -207,6 +299,13 @@ namespace TIENDA.GUI
             ((System.ComponentModel.ISupportInitialize)(this.btnVentas)).EndInit();
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -226,5 +325,12 @@ namespace TIENDA.GUI
         private System.Windows.Forms.ToolStripStatusLabel lblUsuario;
         private System.Windows.Forms.ToolStripStatusLabel lblRol;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
