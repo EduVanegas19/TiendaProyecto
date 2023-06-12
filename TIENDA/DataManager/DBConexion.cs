29425c09a5df7f44aba9b609fc6a87d38eb99a4d
HEEYSANDOVAL <liliosandoval29@gmail.com>
2023-06-11 20:49:48 -0600

    re

--- TIENDA/DataManager/DBConexion.cs
+++ TIENDA/DataManager/DBConexion.cs
@@ -7,7 +7,17 @@ namespace DataManager
     {
         protected SqlConnection _CONEXION = new SqlConnection();
 
+<<<<<<< Updated upstream
         String _CadenaConexion = "Data Source=alfredserver.com;Initial Catalog=Tienda;User ID=OscarAppUser;Password=OQl&@%(R"; 
+=======
+        //static string servidor = "localhost";
+        //static string BD = "Tienda";
+        //static string usuario = "usuario";
+        //static string password = "123";
+        //static string puerto = "1433";
+
+        String _CadenaConexion = "Data Source=alfredserver.com;Initial Catalog=Tienda;User ID=OscarAppUser;Password=OQl&@%(R";
+>>>>>>> Stashed changes
         public static string cn = "Data Source=alfredserver.com;Initial Catalog=Tienda;User ID=OscarAppUser;Password=OQl&@%(R";
 
         public Boolean Conectar()
@@ -20,8 +30,11 @@ namespace DataManager
                 // Accediendo a ServerVersion después de abrir la conexión.
                 string serverVersion = _CONEXION.ServerVersion;
 
+<<<<<<< Updated upstream
 
 
+=======
+>>>>>>> Stashed changes
                 // Haz algo con serverVersion, como imprimirlo a la consola
                 Console.WriteLine(serverVersion);
                 result = true;
